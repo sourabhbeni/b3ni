f2256df7773416ade09a57f7eb05aea3caa62078
SAURABH又BENI <46120445+sourabhbeni@users.noreply.github.com>
2022-03-06 23:19:12 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -60,7 +60,6 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton("Owner", "https://t.me/gc_anon")
-    buttons.buildbutton("Owner", "https://t.me/gc_anon")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
