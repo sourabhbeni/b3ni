7e6bb6af550c50826a1c54e852c724ac1831562f
SAURABH又BENI <46120445+sourabhbeni@users.noreply.github.com>
2022-03-07 17:43:33 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -172,7 +172,7 @@ help_string_telegraph = f'''<br>
 '''
 
 help = telegraph.create_page(
-        title='Mirror-Leech-Bot Help',
+        title='B3NI-Bot Help',
         content=help_string_telegraph,
     )["path"]
 
